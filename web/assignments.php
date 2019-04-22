0e3c164baf2d5bc89fe5e7aad554b71b9bd7ac8c
Alice Smith <smi17100@byui.edu>
2019-04-21 23:25:02 -0600

    Initial setup for week02

--- web/assignments.php
+++ web/assignments.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html>
 <html lang="en-us">
 <head>
-  <title>AS Homepage</title>
+  <title>cs313 Assignments</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta charset="UTF-8"/>
   <link rel="stylesheet" type="text/css" href="week02/week02.css"/>
 </head>
 <body>
     <div class="header">
-      <h2>CS 313 - Prove 02: Personal Homepage</h2>
-      <h1>Idaho Cricket Clubs</h1>
-      <a href="assignments.php">CS 313 - Assignments</a>
+      <h1>CS 313 - Prove 02: Assignments Page</h1>
+      <a href="index.php">Homepage</a>
     </div><hr/>
     <div>
-      <p>
-        Cricket is the best sport in the world! 
-      </p>
-    </div>
+     <h2>Comming Soon</h2>
+    </div><hr/>
 
-    <?php
-      echo "this works from PHP";
-    ?>
-    <hr/>
     <div class="footer">
       <em>20 April 2019 - Alice Smith</em>
     </div>
