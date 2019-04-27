b01704c39ae875e1a06a4d00a73420447f32474d
Alice Smith <smi17100@byui.edu>
2019-04-26 18:30:39 -0600

    basic outline and content.

--- web/index.php
+++ web/index.php
@@ -7,23 +7,61 @@
   <link rel="stylesheet" type="text/css" href="week02/week02.css"/>
 </head>
 <body>
-    <div class="header">
-      <h2>CS 313 - Prove 02: Personal Homepage</h2>
-      <h1>Idaho Cricket Clubs</h1>
-      <a href="assignments.php">CS 313 - Assignments</a>
-    </div><hr/>
-    <div>
-      <p>
-        Cricket is the best sport in the world! 
-      </p>
+   <div class="row">
+    <div class="col-12, header">
+      <h1>Alderan Government</h1>
+      <img src="week02/images/logo.png" alt="Logo"
+    style="height:100px; width:100px;"/>
     </div>
+   </div>
+   <div class="row">
+     <div class="col-12, flex">
+      <div class="menuItem">
+       <a href="assignments.php">Core Policies</a>
+      </div>
+      <div class="menuItem">
+       <a href="assignments.php">Ministers</a>
+      </div>
+      <div class="menuItem">
+       <a href="assignments.php">CS 313 - Assignments</a>
+      </div>
+     </div>
+   </div>
+  
+   <div class="row">
+     <div class="col-3">
+       <h2>Issues</h2>
+       <div class="list">
+         <ul>
+           <li>Privy Council Appointments</li>
+           <li>902nd Annual Royal Athletic Games</li>
+           <li>Alderan Galactic Senate Delegates</li>
+           <li>Royal Succession Laws</li>
+           <li>Trade Union Mining Contracts</li>
+         </ul>
+       </div>
+     </div>
+  
+    <div class="col-9">
+      <h2>Current Ruler</h2>
 
+      <h2>The Royal House of Alderan</h2>
+      <div class="flex">
+        <p>
+          Family History 
+        </p>
+      </div>
+    </div>
+  </div>
     <?php
       echo "this works from PHP";
     ?>
-    <hr/>
-    <div class="footer">
-      <em>20 April 2019 - Alice Smith</em>
+    <div class="row">
+      <div class="col-12, footer">
+        <em>05 May 2019 - Alice Smith - 
+          CS 313 - Prove 02: Personal Homepage
+        </em>
+      </div>
     </div>
 </body>
 </html>
