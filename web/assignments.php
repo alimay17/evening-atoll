e2f7cb0ea51ea168fbe04e915d1a001ac5ff475a
Alice Smith <smi17100@byui.edu>
2019-04-29 21:42:27 -0600

    basic styling

--- web/assignments.php
+++ web/assignments.php
@@ -5,12 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta charset="UTF-8"/>
   <link rel="stylesheet" type="text/css" href="week02/week02.css"/>
+  <link rel="stylesheet" type="text/css" href="week02/week02-2.css"/>
 </head>
 <body>
   <div class="row">
-    <div class="header, col-12">
-      <h1>CS 313 - Prove 02: Assignments Page</h1>
-      <a href="index.php">Homepage</a>
+    <div class="col-12, header">
+      <h1>CS 313 - Assignments Page</h1>
+      <div class="menu, flex">
+        <div class="button"><a href="index.php">Homepage</a></div>
+        <div class="button"><a href="byui.edu">BYU Idaho</a></div>
+        <div class="button">
+          <a href="byui-cs313-19s-04.slack.com">Class Slack Channel<a/> 
+        </div>
+      </div>
     </div>
   </div>
   <div class="row">
