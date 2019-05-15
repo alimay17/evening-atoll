3b58bb3eed427f683fcd00f3abadaed07e870bfd
Alice Smith <smi17100@byui.edu>
2019-05-15 12:19:08 -0600

    added stylesheet and js

--- web/movieReview/header.php
+++ web/movieReview/header.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" type="text/css" href="../grid.css"/>
   
   <!--Javascript & jQuery -->
-  <script src="shopping.js"></script>
+  <script src="movieReview.js"></script>
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
   <script src="../jquery-3.3.1.min.js"></script>
 </head>
