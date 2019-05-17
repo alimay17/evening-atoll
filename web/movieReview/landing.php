1cd52c5c12ad5addd6cb866b9a32ba7838e61291
Alice Smith <smi17100@byui.edu>
2019-05-17 10:38:28 -0600

    UI developments

--- web/movieReview/landing.php
+++ web/movieReview/landing.php
@@ -13,12 +13,16 @@ require('header.php');
   <h2 class="pageTitle">Please select an option</h2>
   <div>
     <ul>
-      <li>View Reviewed Movies</li>
-      <li>Review a movie</li>
-      <li>Submit movie for review</li>
+      <li><a href="viewMovies.php">View Reviewed Movies</a></li>
+      <li><a href="viewMovies.php">Full Movie List</a></li>
+      <li><a href="submitReview.php">Review a movie<a</li>
+      <li><a href="submitMovie.php">Submit movie for review</a></li>
     </ul>
+    <form>
+      <span>Search by Movie Title</span><input type="text"/>
+      <button>Search</button>
+    </form>
   </div>
-
 </div>
 </div>
 
---
+++ web/movieReview/submitMovie.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$pageTitle = "Movie Review";
+require('header.php'); 
+//require('dbAccess.php');
+?>
+
+<!------------------------ MENU -------------------------->
+
+<div class="row">
+<!------------------------ BODY -------------------------->
+<div class="col-12">
+  <h2 class="pageTitle">Submit Movie for Review</h2>
+</div>
+</div>
+
+<!----------------------- FOOTER ------------------------->
+<? require('footer.php'); ?>
