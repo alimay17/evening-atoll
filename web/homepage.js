d3ad33ddea3fd644dd12f3c6cf6e0e6d066dfe7f
Aly Smith <alimay.smith@outlook.com>
2021-12-08 14:57:18 -0600

    minor changes to layout

--- web/homepage.js
+++ web/homepage.js
@@ -1,34 +1,31 @@
 /**********************************************************
-* JavaScript/jQuery Week02: Homepage
-* Alice Smith
-* CS313 - Bro. Porter
-**********************************************************/
+ * JavaScript/jQuery Week02: Homepage
+ * Alice Smith
+ * CS313 - Bro. Porter
+ **********************************************************/
 let fadeswitch = 1;
 
-function show (number) {
+function show(number) {
   var selection = "#" + number;
-    $(selection).fadeToggle();
-    fadeswitch = 0;
+  $(selection).fadeToggle();
+  fadeswitch = 0;
 }
 
 function showAll() {
-  
+
   if (fadeswitch === 1) {
-  $(".card p").fadeToggle();
-  }
-  else {
+    $(".card p").fadeToggle();
+  } else {
     $(".card p").fadeOut();
     fadeswitch = 1;
   }
 }
 
-/**********************************************************
-* getVote
-* makes ajax call to server to process and display poll vote
-**********************************************************/
-function getVote (int) {
-  $.post("vote.php", {vote: int,},
-    function(data){
+function getVote(int) {
+  $.post("vote.php", {
+      vote: int,
+    },
+    function (data) {
       $("#poll").html(data);
     });
 }
